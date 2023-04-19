f965f7a8bae3d90780eeab1278d9be661794698b
Ryan Chen <ryanchen979@gmail.com>
2023-04-19 10:15:39 -0400

    a

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+const covidData = ref("");
+// import TheWelcome from "../components/TheWelcome.vue";
+
+async function api() {
+  let response = await fetch(
+    "https://data.cityofnewyork.us/resource/rc75-m7u3.json"
+  );
+  let data = await response.json();
+  covidData.value = data.death_count;
+  console.log(covidData);
+  // console.log(data.data.death_count[0]);
+  // this.users = users;
+  // console.log(this.users[0]);
+}
+onMounted(() => {
+  api();
+});
 </script>
 
 <template>
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
+  <div></div>
 </template>
+
+<style></style>
