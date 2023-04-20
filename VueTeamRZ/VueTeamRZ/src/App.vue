87dfb22ab1e4344768402a4518999a62634658a1
Vital24 <zynn00001@gmail.com>
2023-04-20 10:16:34 -0400

    AA

--- VueTeamRZ/VueTeamRZ/src/App.vue
+++ VueTeamRZ/VueTeamRZ/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
+  <HomeView></HomeView>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
--- VueTeamRZ/VueTeamRZ/src/views/HomeView.vue
+++ VueTeamRZ/VueTeamRZ/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Chart from 'chart.js/auto'
+import { ref, onMounted } from 'vue'
+const covidData = ref('')
+async function api() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
+  covidData.value = await response.json()
+}
+
+onMounted(() => {
+  api()
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 v-for="data in covidData" :key="data">{{ covidData.death_count }}</h1>
   </main>
 </template>
