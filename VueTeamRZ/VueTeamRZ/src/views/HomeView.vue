bc13d7866882b4e83974ca8318cb88ef21f965d3
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-05-01 19:43:54 -0400

    AA

---
+++ VueTeamRZ/VueTeamRZ/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+// import Chart from 'chart.js/auto'
+
+/* import { ref, onMounted } from 'vue'
+const covidData = ref('')
+async function covidAPI() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
+  covidData.value = await response.json()
+}
+
+onMounted(() => {
+  covidAPI()
+  console.log('This good?')
+}) */
+</script>
+
+<template>
+  <main>
+    <!-- <div v-for="data in covidData" :key="data">
+      <h1>{{ data.death_count }} Deaths on {{ data.date_of_interest }}</h1>
+      <h1>{{ data.bx_death_count }} in Bronx</h1>
+      <h1>{{ data.bk_death_count }} in Brooklyn</h1>
+      <h1>{{ data.mn_death_count }} in Manhattan</h1>
+      <h1>{{ data.qn_death_count }} in Queens</h1>
+      <h1>{{ data.si_death_count }} in Staten Island</h1>
+    </div> -->
+  </main>
+</template>
